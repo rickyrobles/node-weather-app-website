985bc89bc0d569be7d3439bcb2610536c24ac5b7
Shella Dejucos <shelladejucos@gmail.com>
2021-11-03 18:35:32 +0800

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=23f2ff465f6bc64a593fbb9993558517&query=' + latitude + ',' + longitude + '&units=f'
+
+    request({ url, json: true }, (error, { body }) =>{
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback ('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out.")
+
+        }
+    })
+
+
+}
+
+module.exports = forecast
